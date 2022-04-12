5656daeab1d9c872a7ede74340217d7116982920
publiminal <publiminal@gmail.com>
2022-04-12 00:56:41 -0400

    working classes and external data loaded

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import Form from './Form'
+import TodoList from './TodoList'
+import axios from 'axios'
 
 export default class App extends React.Component {
+    constructor(){
+      super()
+      this.state={todos:[]}
+    }
+   
+    getTodos = () => {
+      axios.get('http://localhost:9000/api/todos')
+        .then(res => {
+           console.log('data', res.data.data)
+           this.setState({todos:res.data.data} )
+        }).catch(err => console.error(err))
+    }
+    
+    /* on first render calling external data */
+    componentDidMount(){
+      this.getTodos()
+    }
+  
   render() {
+
+
     return (
       <div>
-        Todo App
+        {/* {!this.state.todos.length && <div>loading data...</div> } */}
+        <TodoList todos={this.state.todos}  />
+        <Form />
       </div>
     )
   }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
+/*   constructor(props){
+    super(props)
+  } */
+
+
+
+
   render() {
     return (
-      <div>
-        TodoList
+      <div id='todos'>     
+        {this.props.todos.map( todo => (
+          // console.log(todo.name)
+           <Todo id={todo.id} info={todo.name} completed={todo.completed} />
+         ))}
       </div>
     )
   }
